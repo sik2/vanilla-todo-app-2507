d15b37391d58796bbef63a342ff5903f6c102eaa
sik2 <bickwang123@gmail.com>
2025-07-07 11:31:13 +0900

    todoform

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import TodoList from "./components/TodoList.js";
 function App({ $target }) {
   const intialState = ["할일1", "할일2", "할일3"];
 
+  const onSubmit = (text) => {
+    const nextState = [...todoList.state, text];
+    todoList.setState(nextState);
+  };
+
   const $page = document.createElement("div");
   $target.appendChild($page);
 
   const header = new Header({ $target: $page });
   header.setState("!!오늘 할일!!");
 
-  new TodoForm({ $target: $page });
+  new TodoForm({ $target: $page, onSubmit });
 
-  const list = new TodoList({ $target: $page, intialState });
-  list.setState([...intialState, "할일4"]);
+  const todoList = new TodoList({ $target: $page, intialState });
 }
 
 export default App;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,30 @@
-function TodoForm({ $target }) {
+function TodoForm({ $target, onSubmit }) {
   const $form = document.createElement("form");
   $form.innerText = "할일 등록 폼";
-  $form.innerHTML = `<input /><button>등록</button>`;
   $target.appendChild($form);
+
+  //   this.state;
+
+  //   this.setState = () => {};
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const $input = $form.querySelector("input");
+    const text = $input.value;
+
+    if (text.length > 0) {
+      onSubmit(text);
+    }
+  });
+
+  this.render = () => {
+    $form.innerHTML = `
+        <input type="text" />
+        <button>등록</button>
+    `;
+  };
+
+  this.render();
 }
 
 export default TodoForm;
