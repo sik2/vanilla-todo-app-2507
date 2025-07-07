2caffd1d20ee9f46d06be2cb45d5232759bb7086
sik2 <bickwang123@gmail.com>
2025-07-07 10:42:48 +0900

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header.js";
 import TodoForm from "./components/TodoForm.js";
 import TodoList from "./components/TodoList.js";
 
 function App({ $target }) {
   const $page = document.createElement("div");
   $target.appendChild($page);
+
+  const header = new Header({ $target: $page });
+  header.setState("!!오늘 할일!!");
 
   new TodoForm({ $target: $page });
   new TodoList({ $target: $page });
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header({ $target }) {
+  const $header = document.createElement("h1");
+  $target.appendChild($header);
+
+  this.state = "오늘 할일";
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $header.textContent = this.state;
+  };
+
+  this.render();
+}
+
+export default Header;
