e724cf844574d658f4526a7b42e56dbc03d71330
sik2 <bickwang123@gmail.com>
2025-07-07 09:39:55 +0900

    App

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App({ $target }) {
+  console.log($target);
+}
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import App from "./App.js";
+
 const $root = document.getElementById("root");
 
-console.log($root);
+new App({ $target: $root });
